4881e786a02171a2fd1447086b0bf4a836abbfe5
Gabriel <gabrielmartinsdelfes3@gmail.com>
2023-09-09 16:04:30 -0300

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -5,93 +5,91 @@ def criar_tabuleiro(dimensao):
     """Cria o tabuleiro do jogo da velha."""
     return [["-" for _ in range(dimensao)] for _ in range(dimensao)]
 
-def imprimir_tabuleiro(dif):
+def imprimir_tabuleiro(dimensao):
     """Imprime o tabuleiro do jogo da velha."""
-    for i in tabuleiro:
-        tab_final = " | ".join(i)
-        print(tab_final)
-        print("--+--"*(dif-1))
+    for linha in tabuleiro:
+        linha_formatada = " | ".join(linha)
+        print(linha_formatada)
+        print("--+--" * (dimensao - 1))
 
-
-def ganhou(jogador, dimensao):
+def verificar_vitoria(jogador, dimensao):
     """
     Verifica se o jogador ganhou o jogo.
-    Verifica se ganhou o jogo em linha.
-    Verifica se ganhou o jogo em diagonal.
-    Verifica se ganhou o jogo em coluna.
+    Verifica a vitória nas linhas, colunas e diagonais.
     """
-    
-    for i in tabuleiro: #linha
-        if all(x == jogador for x in i):
+    for linha in tabuleiro:  # Linhas
+        if all(casa == jogador for casa in linha):
             return True
-    
-    for j in range(dimensao): #coluna
-        if all(tabuleiro[l][j] == jogador for l in range(dimensao)):
+
+    for coluna in range(dimensao):  # Colunas
+        if all(tabuleiro[linha][coluna] == jogador for linha in range(dimensao)):
             return True
-    
-    if all(tabuleiro[i][i] == jogador for i in range(dimensao) or tabuleiro[i][dimensao-i] == jogador for i in range(dimensao)): #diagonal
+
+    if all(tabuleiro[i][i] == jogador for i in range(dimensao)) or all(
+        tabuleiro[i][dimensao - i - 1] == jogador for i in range(dimensao)
+    ):  # Diagonais
         return True
     return False
 
-def empate(dimensao, jogadas):
-    """Verifica se teve empate"""
-    if jogadas == dimensao*dimensao:
+def verificar_empate(dimensao, jogadas):
+    """Verifica se houve empate."""
+    if jogadas == dimensao * dimensao:
         return True
     else:
         return False
-                
+
 def jogar_jogo(dimensao):
     """Executa o jogo"""
     jogadas = 1
     jogador1 = "X"
     jogador2 = "O"
     while True:
-        print(f"Vez de Jogador {1 if jogadas%2 == 1 else 2}.")
+        print(f"Vez do Jogador {1 if jogadas % 2 == 1 else 2}.")
         imprimir_tabuleiro(dimensao)
         try:
             linha = int(input("Digite a linha: "))
             coluna = int(input("Digite a coluna: "))
-            if(linha < 1 or linha > dimensao or coluna < 1 or coluna > dimensao ):
+            if linha < 1 or linha > dimensao or coluna < 1 or coluna > dimensao:
                 print("Fora de dimensão!")
                 time.sleep(2)
-                os.system('cls')
+                os.system("cls")
                 continue
 
         except ValueError:
             print("Digite um número!")
             time.sleep(2)
-            os.system('cls')
+            os.system("cls")
         else:
-            
-            if(tabuleiro[linha-1][coluna-1] != "-"):
-                print("Está casa já está ocupada.")
+
+            if tabuleiro[linha - 1][coluna - 1] != "-":
+                print("Esta casa já está ocupada.")
                 time.sleep(2)
                 continue
             else:
-                if(jogadas%2==0):
-                    tabuleiro[linha-1][coluna-1] = jogador2
+                if jogadas % 2 == 0:
+                    tabuleiro[linha - 1][coluna - 1] = jogador2
                 else:
-                    tabuleiro[linha-1][coluna-1] = jogador1
-            
-            if(ganhou(jogador1 if jogadas%2 == 1 else jogador2, dimensao)):
+                    tabuleiro[linha - 1][coluna - 1] = jogador1
+
+            if verificar_vitoria(
+                jogador1 if jogadas % 2 == 1 else jogador2, dimensao
+            ):
                 imprimir_tabuleiro(dimensao)
-                print(f"Parabéns Jogador {jogadas%2 + 1}, você ganhou!")
+                print(f"Parabéns Jogador {jogadas % 2 + 1}, você ganhou!")
                 time.sleep(2)
                 break
             jogadas += 1
-            if(empate(dimensao, jogadas)):
+            if verificar_empate(dimensao, jogadas):
                 imprimir_tabuleiro(dimensao)
                 print("Empate!")
                 time.sleep(2)
                 break
-            os.system('cls')
+            os.system("cls")
 
-            
-dimensao = int(input("Digite o tamanho da dimensão: "))
+
+dimensao = int(input("Digite o tamanho da dimensão (deve ser 3 ou mais): "))
 while dimensao < 3:
-    dimensao = int(input("Tamanho muito pequeno.\nDigite o tamanho da dimensão: "))
-    
+    dimensao = int(input("Tamanho muito pequeno.\nDigite o tamanho da dimensão (deve ser 3 ou mais): "))
 
 tabuleiro = criar_tabuleiro(dimensao)
 jogar_jogo(dimensao)
-
