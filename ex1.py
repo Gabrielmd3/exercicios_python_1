3ebb14bec3b3c5d83780fa6c9966b66ce366feb2
Gabriel <gabrielmartinsdelfes3@gmail.com>
2023-09-09 16:00:50 -0300

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -1,89 +1,82 @@
 import time
 
-
 tabuleiro = [
-    ["-","-","-","-"],
-    ["-","-","-","-"],
-    ["-","-","-","-"],
-    ["-","-","-","-"]
+    ["-", "-", "-", "-"],
+    ["-", "-", "-", "-"],
+    ["-", "-", "-", "-"],
+    ["-", "-", "-", "-"]
 ]
 
 def imprimir_tabuleiro():
-    """Imprime o tabuleiro do jogo da velha."""
-    for i in tabuleiro:
-        tab_final = " | ".join(i)
-        print(tab_final)
+    """Exibe o tabuleiro do jogo da velha."""
+    for linha in tabuleiro:
+        linha_formatada = " | ".join(linha)
+        print(linha_formatada)
         print("--+---+---+---")
 
-
-def ganhou(jogador):
+def verificar_vitoria(jogador):
     """
-    Verifica se o jogador ganhou o jogo.
-    Verifica se ganhou o jogo em linha.
-    Verifica se ganhou o jogo em diagonal.
-    Verifica se ganhou o jogo em coluna.
+    Verifica se o jogador venceu o jogo.
+    Verifica a vitória nas linhas, colunas e diagonais.
     """
-    #linha
-    for i in tabuleiro:
-        if all(x == jogador for x in i):
+    # Linhas
+    for linha in tabuleiro:
+        if all(casa == jogador for casa in linha):
             return True
-    #coluna
-    for j in range(4):
-        if all(tabuleiro[l][j] == jogador for l in range(4)):
+    # Colunas
+    for coluna in range(4):
+        if all(tabuleiro[linha][coluna] == jogador for linha in range(4)):
             return True
-    #diagonal
-    if all(tabuleiro[i][i] == jogador for i in range(4) or tabuleiro[i][4-i] == jogador for i in range(4)):
+    # Diagonais
+    if all(tabuleiro[i][i] == jogador for i in range(4)) or all(tabuleiro[i][3 - i] == jogador for i in range(4)):
         return True
     return False
 
-def empate(jogadas):
-    """Verifica se teve empate"""
-    if jogadas == 4*4:
+def verificar_empate(jogadas):
+    """Verifica se houve empate."""
+    if jogadas == 4 * 4:
         return True
     else:
         return False
                 
 def jogar_jogo():
-    """Executa o jogo"""
+    """Executa o jogo."""
     jogadas = 1
     jogador1 = "X"
     jogador2 = "O"
     while True:
-        print(f"Vez de Jogador {jogadas%2 + 1}.")
+        print(f"Vez do Jogador {jogadas % 2 + 1}.")
         imprimir_tabuleiro()
         try:
-            linha = int(input("Digite a linha: "))
-            coluna = int(input("Digite a coluna: "))
-            if(linha < 1 or linha > 4 or coluna < 1 or coluna > 4 ):
+            linha = int(input("Digite a linha (1-4): "))
+            coluna = int(input("Digite a coluna (1-4): "))
+            if linha < 1 or linha > 4 or coluna < 1 or coluna > 4:
                 print("Fora de dimensão!")
                 time.sleep(2)
                 continue
         except ValueError:
             print("Digite um número!")
         else:
-            
-            if(tabuleiro[linha-1][coluna-1] != "-"):
-                print("Está casa já está ocupada.")
+            if tabuleiro[linha - 1][coluna - 1] != "-":
+                print("Esta casa já está ocupada.")
                 time.sleep(2)
                 continue
             else:
-                if(jogadas%2==0):
-                    tabuleiro[linha-1][coluna-1] = jogador2
+                if jogadas % 2 == 0:
+                    tabuleiro[linha - 1][coluna - 1] = jogador2
                 else:
-                    tabuleiro[linha-1][coluna-1] = jogador1
+                    tabuleiro[linha - 1][coluna - 1] = jogador1
             
-            if(ganhou(jogador1 if jogadas%2 == 1 else jogador2)):
+            if verificar_vitoria(jogador1 if jogadas % 2 == 1 else jogador2):
                 imprimir_tabuleiro()
-                print(f"Parabéns Jogador {jogadas%2 + 1}, você ganhou!")
+                print(f"Parabéns Jogador {jogadas % 2 + 1}, você ganhou!")
                 time.sleep(2)
                 break
             jogadas += 1
-            if(empate(jogadas)):
+            if verificar_empate(jogadas):
                 imprimir_tabuleiro()
                 print("Empate!")
                 time.sleep(2)
                 break
-            
+
 jogar_jogo()
-
-
